1521827fba84288e0a5c614a1ef01c940b37fb2a
Josh Panebianco <pana23@gmail.com>
2016-04-05 09:28:14 +1000

    Homework for Monday

---
+++ JoshuaPanebianco/week_07/movies/js/main.js
@@ -0,0 +1,50 @@
+// where is the info
+// what is the URL
+
+// call of this
+
+// click handler for click of search button
+// then .value the search result
+
+// var userInput = "";
+//
+// var url ="http://omdbapi.com?t=" + userInput;
+//
+// var request = new XMLHttpRequest();
+// request.open( "GET", url);
+// request.send();
+//
+// request.onreadystatechange = function () {
+//   if (request.readyState === 4) {
+//     var info = JSON.parse ( request.responseText )
+//     console.log(request.responseText['Title']);
+//   console.log ( request.readyState);
+//   }
+// };
+
+$(document).ready(function () {
+
+  var getMovie = function (event) {
+    event.preventDefault();
+    var userInput = $('#movieTitle').val();
+
+
+
+    var url ="http://omdbapi.com?t=" + userInput;
+
+
+    var request = new XMLHttpRequest();
+    request.open( "GET", url);
+    request.send();
+
+    request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        var info = JSON.parse( request.responseText )
+        console.log(info['Title']);
+      console.log ( request.readyState);
+      }
+    };
+  }
+
+  $('button').on('click', getMovie);
+});
